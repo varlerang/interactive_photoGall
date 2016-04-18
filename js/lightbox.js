99f754f1053863df17dec790dd40c1a9e0b09751
varlerang <varlerang@gmail.com>
2016-04-17 22:16:31 -0400

    there has been some changes to the js file trying to allow the image to show on the overlay.

--- js/lightbox.js
+++ js/lightbox.js
@@ -2,6 +2,39 @@
 
   // appending div to the body //
   
-    $overlay = $("<div id='overlay'></div>");
+   
+	  
+  
+     var $image = $("<img>");
+	 
+	  
+  
+     var $overlay = $('<div id="overlay"></div>');
 	
+	  $overlay.append($image);
+	  
 	 $("body").append($overlay);
+	 
+	 
+	  $(".pic-nav li ").click( function(e) {
+		  
+		   e.preventDefault();
+		   
+		   var loc = $(this).attr("src");
+		   
+		    $image.attr("src", loc);
+			
+			$overlay.show();
+		  
+		  
+	  });
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+
+	  
+	 
