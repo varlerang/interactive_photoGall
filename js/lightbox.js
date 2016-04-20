5d4fd80d08d470265e94d9469113915b4d4b4001
varlerang <varlerang@gmail.com>
2016-04-20 03:40:32 -0400

    the overlay has the img and content text show with a click of an image.
    the overlay has a display: flex
    and its center. also in the css the p tag has a colour of white which
    is centered along with the image with a margin top.//
    
      html changed with the alt attr added to the img tag and deleted the
    alt of the a tag//
    
     in the js file I changed the code which I added a var $para which
    holds the content . and appended to the overlay before the image///
    created a var called captionTex in the  function  that cache the string
    that captures the alt attr of the img tag.

--- js/lightbox.js
+++ js/lightbox.js
@@ -2,31 +2,61 @@
 
   // appending div to the body //
   
-   
-	  
+  
+	  var $para = $("<p></p>");
   
      var $image = $("<img>");
 	 
-	  var $overlay = $('<div id="overlay"></div>');
+	  var $overlay = $("<div id='overlay'></div>");
+	  
+	  
+	  
+	  
+	  
+	  //				 AdDiNg OvErLaY aNd iMaGe WiTh TeXt 			//
+	  
+	  
+	  
+	  
+	 
 	
 	  $overlay.append($image);
 	  
+	  $overlay.append($para);
+	  
 	 $("body").append($overlay);
+	 
+	 
+	 // iMaGe CliCk FuNcTiOn ShOwInG oVeRlAy			//
 	 
 	 
 	  $(".pic-nav li a").click( function(e) {
 		  
 		   e.preventDefault();
 		   
-		   var loc = $(this).attr("href");
-		 
-		    $image.attr("src", loc);
+		   var imageLoc = $(this).attr("href");
+		     
+			
+		
+		    $image.attr("src", imageLoc);
+			
 			
 			$overlay.show();
 			
+			
+			
+			 var captionTex = $(this).children("img").attr("alt");
+		
+			 $para.text(captionTex);
+			
+			
 	 });
 		  
-		  
+		 $image.click( function() {
+			 
+			 $overlay.hide(); 
+			 
+		 });
 		
 		    
 		  
